9c61a7f496799a0bd64dd7239d6b0452ebbf477c
root <root@training3.training3>
2016-11-14 22:58:39 -0800

    update test.js

--- test.js
+++ test.js
@@ -6,3 +6,30 @@ module.exports = [
         "data": {}
     }
 ]
+
+module.exports = [
+    {
+        "api": "/crmAms/dailyAppointmentsSetAjax?store_id=1&date=2016-08-27",
+        "method": "get",
+        "headers": {},
+        "data": {}
+    }
+]
+
+module.exports = [
+    {
+        "api": "/crmAms/findAppointmentsSetAjax?store_id=1&name=cccccc",
+        "method": "get",
+        "headers": {},
+        "data": {}
+    }
+]
+
+module.exports = [
+    {
+        "api": "/crmAms/findAppointmentCancelLogsSetAjax?store_id=1&name=aa",
+        "method": "get",
+        "headers": {},
+        "data": {}
+    }
+]
